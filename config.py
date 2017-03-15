d0832d6dd529290f207594b502443de6250d92ba
czk <chenzikunczk@gmail.com>
2017-03-15 16:37:07 +0800

    第一次提交

---
+++ config.py
@@ -0,0 +1,41 @@
+
+# 真实环境
+# MYSQL_CONFIG_OUTLINE = {
+#     'host': '192.168.1.158',
+#     'port': 20002,
+#     'user': 'root',
+#     'password': 'xwroot*555',
+#     'db': 'xw',
+#     'charset': 'utf8mb4'
+# }
+#
+#
+# MYSQL_CONFIG_ONLINE = {
+#     'host': '10.47.140.182',
+#     'port': 23307,
+#     'user': 'ppw',
+#     'password': 'ppwdb1779',
+#     'db': 'xw',
+#     'charset': 'utf8mb4'
+# }
+
+
+# 测试环境
+MYSQL_CONFIG_ONLINE = {
+    'host': '192.168.1.158',
+    'port': 20002,
+    'user': 'root',
+    'password': 'xwroot*555',
+    'db': 'xw',
+    'charset': 'utf8mb4'
+}
+
+
+MYSQL_CONFIG_OUTLINE = {
+    'host': '192.168.1.253',
+    'port': 20002,
+    'user': 'chenzikun',
+    'password': 'ppwchenzikun',
+    'db': 'xw',
+    'charset': 'utf8mb4'
+}
